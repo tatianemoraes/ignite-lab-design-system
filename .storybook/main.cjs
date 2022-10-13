d598adc322a69b08feb06d4580c4d75ec3b19439
Tatiane Moraes <tatiane.moraes@take.net>
2022-10-12 23:33:12 -0300

    fix: pipeline and production config

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -12,11 +12,4 @@ module.exports = {
   features: {
     storyStoreV7: true,
   },
-  viteFinal: (config, { configType }) => {
-    if (configType === "PRODUCTION") {
-      config.base = "/ignite-lab-design-system";
-    }
-
-    return config;
-  },
 };
